233be462234b30c930366eab8821d6a85d5930f1
zaheershaik <zaheershaik0323@gmail.com>
2025-06-11 08:00:47 +0530

    OopsUpdated

--- LabWork/Oops/PolyMerphism/OverLoading.cpp
+++ LabWork/Oops/PolyMerphism/OverLoading.cpp
@@ -1,26 +1,27 @@
 #include<iostream>
 using namespace std;
+
 class calculater {
 public:
-    void add(int A, int B, int C=0){
+    void add(int A, int B, int C=0) {
         if (C==0) {
-            cout<<"The sum of Two Numbers int a+b = "<<A+B+C<<endl;
+            cout<<"The Sum of Two Int Numbers = "<<A+B+C<<endl;
         }else {
-            cout<<"The sum of Three Numbers int a+b+c = "<<A+B+C<<endl;
+            cout<<"The Sum of Three Int Numbers = "<<A+B+C<<endl;
         }
     }
     void add(float A, float B, float C=0) {
         if (C==0) {
-            cout<<"The sum of Two Numbers int a+b = "<<A+B+C<<endl;
+            cout<<"The Sum of Two Int Numbers = "<<A+B+C<<endl;
         }else {
-            cout<<"The sum of Three Numbers int a+b+c = "<<A+B+C<<endl;
+            cout<<"The Sum of Three Int Numbers = "<<A+B+C<<endl;
         }
     }
 };
 int main() {
-    calculater sum;
-    sum.add(10,5);
-    sum.add(30,40,50);
-    sum.add(30.5F,60.56F);
-    sum.add(70.45F,67.34F,78.90F);
+    calculater cal;
+    cal.add(10,5);
+    cal.add(20,5070,80);
+    cal.add(90.56F,89.09F);
+    cal.add(56.90F, 67.89F,556.78F);
 }
