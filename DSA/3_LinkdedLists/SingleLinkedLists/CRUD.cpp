8e47f1922c76aa391610afe20a56be7cd0aa9614
iamzaheershaik <zaheershaik0323@gmail.com>
2025-07-01 14:44:50 +0530

    LinkedListedOperations

--- DSA/3_LinkdedLists/SingleLinkedLists/CRUD.cpp
+++ DSA/3_LinkdedLists/SingleLinkedLists/CRUD.cpp
@@ -15,3 +15,27 @@ class ListNode {
         }
         return count;
     }
+    int ListInsert(ListNode *head, int data, int position) {
+        int k=1;
+        ListNode *p, *q, *newNode;
+        ListNode* newNode = new ListNode();
+        if (!newNode) {
+            cout<<"memory error"<<endl;
+            return;
+        }
+        newNode->data =data;
+        p=*head;
+        if (position==1) {
+            newNode->next=p;
+            *head=newNode;
+        }else {
+            while ((p!=NULL) && (k<position)){
+                k++;
+                q=p;
+                p=p->next;
+            }
+            q->next=nextNode;
+            newNode->next=p;
+        }
+
+    }
